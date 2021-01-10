1453690cedea8d84c4b20d7798581ca1c454c7c5
Roguez84 <t.roguez984@gmail.com>
2021-01-10 19:56:40 -0300

    desafio rentabilidad ready for first submission

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -1,12 +1,15 @@
 
-puts "Ingrese precio"
-precio_venta = gets.chomp.to_f
+#puts "Ingrese precio"
+#precio_venta = gets.chomp.to_f
+precio_venta = ARGV[0].to_f
 
-puts "Ingrese # de usuarios"
-usuarios = gets.chomp.to_f
+#puts "Ingrese # de usuarios"
+#usuarios = gets.chomp.to_f
+usuarios = ARGV[1].to_f
 
-puts "Ingrese gastos"
-gastos = gets.chomp.to_f
+#puts "Ingrese gastos"
+#gastos = gets.chomp.to_f
+gastos = ARGV[2].to_f
 
 utilidades = precio_venta*usuarios - gastos
 puts "la utilidad es $#{utilidades}" 
--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,18 +1,19 @@
 
-puts "Ingrese precio"
-precio_venta = gets.chomp.to_f
+#puts "Ingrese precio"
+precio_venta = ARGV[0].to_f
 
-puts "Ingrese # de usuarios"
-usuarios = gets.chomp.to_f
+#puts "Ingrese # de usuarios"
+usuarios = ARGV[1].to_f
 
-puts "Ingrese # de usuarios premium"
-usuarios_premium = gets.chomp.to_f
+#puts "Ingrese # de usuarios premium"
+usuarios_premium = ARGV[2].to_f
 
-puts "Ingrese # de usuarios gratuitos"
-usuarios_gratuitos = gets.chomp.to_f
+#puts "Ingrese # de usuarios gratuitos"
+usuarios_gratuitos = ARGV[3].to_f
 
-puts "Ingrese gastos"
-gastos = gets.chomp.to_f
+#puts "Ingrese gastos"
+gastos = ARGV[4].to_f
 
-utilidades = (precio_venta*usuarios+precio_venta*2*usuarios_premium-precio_venta*usuarios_gratuitos) - gastos
+
+utilidades = ((precio_venta*usuarios)+(precio_venta*2*usuarios_premium)-(precio_venta*0*usuarios_gratuitos)) - gastos
 puts "la utilidad es $#{utilidades}" 
