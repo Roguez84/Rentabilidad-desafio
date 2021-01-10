89b8133bdf5cae95c5502f2c6d91c3770827a4a3
Roguez84 <t.roguez984@gmail.com>
2021-01-10 20:05:38 -0300

    desafio rentabilidad added tax calculation

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -11,5 +11,12 @@ usuarios = ARGV[1].to_f
 #gastos = gets.chomp.to_f
 gastos = ARGV[2].to_f
 
-utilidades = precio_venta*usuarios - gastos
+utilidades_pre_tax = precio_venta*usuarios - gastos
+utilidades = nil
+
+if utilidades_pre_tax > 0
+    utilidades = utilidades_pre_tax*(1-0.35)
+else
+    utilidades = utilidades_pre_tax
+end
 puts "la utilidad es $#{utilidades}" 
--- emprendedor2.rb
+++ emprendedor2.rb
@@ -15,5 +15,12 @@ usuarios_gratuitos = ARGV[3].to_f
 gastos = ARGV[4].to_f
 
 
-utilidades = ((precio_venta*usuarios)+(precio_venta*2*usuarios_premium)-(precio_venta*0*usuarios_gratuitos)) - gastos
+utilidades_pre_tax = ((precio_venta*usuarios)+(precio_venta*2*usuarios_premium)-(precio_venta*0*usuarios_gratuitos)) - gastos
+utilidades = nil
+
+if utilidades_pre_tax > 0
+    utilidades = utilidades_pre_tax*(1-0.35)
+else
+    utilidades = utilidades_pre_tax
+end
 puts "la utilidad es $#{utilidades}" 
